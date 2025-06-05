ed383d1c9ee8b2e4ed928e28f6e4a47b650d60d3
ROOPUN LEERISH <LEERISH.RPN@ge-pedago.etat-ge.ch>
2025-06-05 16:02:23 +0200

    ajout du fichier Mouette.java

---
+++ Mouette.java
@@ -0,0 +1,26 @@
+public class Mouette extends Véhicule {
+    private int nombreDeGiletsDeSauvetage;
+
+
+    public Mouette(int nombreDePassagersMaximum, double prixAchat, int nombreDeGiletsDeSauvetage) {
+        super(nombreDePassagersMaximum, prixAchat);
+
+        if (nombreDeGiletsDeSauvetage > nombreDePassagersMaximum) {
+            this.nombreDeGiletsDeSauvetage = nombreDeGiletsDeSauvetage;
+        } else {
+            throw new IllegalArgumentException("Pas assez de gilets, création impossible");
+        }
+        //this.nombreDeGiletsDeSauvetage = nombreDeGiletsDeSauvetage;
+    }
+
+
+    public void setNombreDeGiletsDeSauvetage(int nombreDeGiletsDeSauvetage) {
+        this.nombreDeGiletsDeSauvetage = nombreDeGiletsDeSauvetage;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " de type:  Mouette avec " + nombreDeGiletsDeSauvetage + (nombreDeGiletsDeSauvetage > getNombreDePassagersMaximum() ? " gilets de secours" : " gilets de secours (PAS ASSEZ DE GILETS!)");
+
+    }
+}
